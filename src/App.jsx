52db1731902916c407f55e786314d57ac69e1a3e
mayoshmase-dot <mayoshmase@gmail.com>
2026-01-17 01:57:50 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './assets/header/Header'
+import Hero from './assets/hero/Hero'
+import Portfolio from './assets/portfolio/Portfolio'
+import About from './assets/about/About'
+import Contact from './assets/contact/Contact'
+import Footer from './assets/footer/Footer'
+
+function App() {
+  return <>
+    <Header />
+    <Hero />
+    <Portfolio/>
+    <About/>
+    <Contact/>
+    <Footer/>
+  </>
+}
+export default App;
