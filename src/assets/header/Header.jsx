94b7be027e4811f03842c9be58fca5c88e37708d
mayoshmase-dot <mayoshmase@gmail.com>
2026-01-17 03:38:05 +0200

    portfolio section

--- src/assets/header/Header.jsx
+++ src/assets/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from './header.module.css'
 export default function Header() {
   return ( <>
-<nav className={`${style.demo} navbar navbar-expand-lg p-4 position-sticky top-0`}>
+<nav className={`${style.demo} navbar navbar-expand-lg p-4 position-sticky top-0 z-1`}>
   <div className="container ">
     <a className="navbar-brand text-white" href="#">Start BootStrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/assets/portfolio/Portfolio.jsx
+++ src/assets/portfolio/Portfolio.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
-
+import port1 from "../imgs/port1.png"
+import port2 from "../imgs/port2.png"
+import port3 from "../imgs/port3.png"
+import port4 from "../imgs/port4.png"
+import port5 from "../imgs/port5.png"
+import port6 from "../imgs/port6.png"
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <section className='my-5 '>
+      <div className='container'>
+        <h2 className='text-center'>Portfolio</h2>
+        <div className='row '>
+          <div className='col-md-4 mt-5'>
+            <div className="card" >
+              <img src={port1} className="card-img-top" />
+            </div>
+
+          </div>
+          <div className='col-md-4 mt-5'>
+            <div className="card">
+              <img src={port2} className="card-img-top" />
+            </div>
+
+          </div><div className='col-md-4 mt-5'>
+            <div className="card">
+              <img src={port3} className="card-img-top" />
+            </div>
+
+          </div><div className='col-md-4 mt-5'>
+            <div className="card">
+              <img src={port4} className="card-img-top" />
+            </div>
+
+          </div><div className='col-md-4 mt-5'>
+            <div className="card">
+              <img src={port5} className="card-img-top" />
+            </div>
+
+          </div><div className='col-md-4 mt-5'>
+            <div className="card">
+              <img src={port6} className="card-img-top" />
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
