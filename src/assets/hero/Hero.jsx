67e03b2e5c114e53354065e6edfc9203828010ad
mayoshmase-dot <mayoshmase@gmail.com>
2026-01-17 03:20:38 +0200

    hero section

--- src/assets/hero/Hero.jsx
+++ src/assets/hero/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import style from './hero.module.css'
+import logo from "../imgs/Logo.svg"
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+<div className={`${style.hero} d-flex flex-column gap-4 justify-content-center align-items-center  `}>
+<img src={logo} className='w-25 ' alt="" />
+<h1>Start Bootstrap</h1>
+<h3>Graphic Artist - Web Designer - Illustrator</h3>
+</div> )
 }
