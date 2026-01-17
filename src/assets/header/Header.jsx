7d1857ef2693cbf02c24ecdb79dc1984b7a71eac
mayoshmase-dot <mayoshmase@gmail.com>
2026-01-17 02:52:48 +0200

    header section

--- src/assets/header/Header.jsx
+++ src/assets/header/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import style from './header.module.css'
 export default function Header() {
-  return (
-    <div className='text-white'>Header</div>
+  return ( <>
+<nav className={`${style.demo} navbar navbar-expand-lg p-4 position-sticky top-0`}>
+  <div className="container ">
+    <a className="navbar-brand text-white" href="#">Start BootStrap</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse " id="navbarNav">
+      <ul className='navbar-nav ms-auto d-flex gap-5  '>
+        <li className="nav-item ">
+          <a className="nav-link text-white " aria-current="page" href="#">Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
   )
 }
